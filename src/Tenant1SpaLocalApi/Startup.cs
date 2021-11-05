1e32b6ed325e0751b08e766c9017483d6ff7f4e3
ChristianKlecina <djfntt@yahoo.com>
2021-11-05 23:15:39 +0100

    Refactor

--- src/Tenant1SpaLocalApi/Startup.cs
+++ src/Tenant1SpaLocalApi/Startup.cs
@@ -28,7 +28,7 @@ namespace Tenant1SpaPolymer
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            //loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            
             //loggerFactory.AddDebug();
 
             // this sample is calling the api/identity in the main sourceDev.WebApp project
