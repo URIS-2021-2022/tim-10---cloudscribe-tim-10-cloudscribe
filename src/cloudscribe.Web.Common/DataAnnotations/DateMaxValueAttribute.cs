174b4e64239c4ca668625dd0c8bc3e44aaef58c5
teaajo <teodora.jovanovic7063@uns.ac.rs>
2021-11-12 12:56:46 +0100

    Refactoring7

--- src/cloudscribe.Web.Common/DataAnnotations/DateMaxValueAttribute.cs
+++ src/cloudscribe.Web.Common/DataAnnotations/DateMaxValueAttribute.cs
@@ -65,7 +65,7 @@ namespace cloudscribe.Web.Common.DataAnnotations
             catch (Exception ex)
             {
 
-                throw new Exception ( "Not valid! Error: ", ex);
+                throw new ArgumentNullException(ex.ToString());
             }
 
             return validationResult;
